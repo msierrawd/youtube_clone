5491adbe81cf6d0dd725f696beca7102a2b5d8c5
Msierra001 <msierra001@citymail.cuny.edu>
2025-07-10 22:50:58 -0400

    Addition of error checking for inputFilePath and outputFilePath

--- video_processing_service/src/index.ts
+++ video_processing_service/src/index.ts
@@ -9,9 +9,20 @@ app.post("/process-video" , (req, res) => {
     const inputFilePath = req.body.inputFilePath;
     const outputFilePath = req.body.outputFilePath; 
 
-
+    // error checking inputFilePath and outputFilePath for when they're individually missing 
+    // or when they're both missing
+    if(!inputFilePath && !outputFilePath){
+        res.status(400).send("Bad Request: Missing input and output file path");
+    } 
+    else if(!inputFilePath){
+        res.status(400).send("Bad Request: Missing input file path.");
+    }
+    else if(!outputFilePath){
+        res.status(400).send("Bad Request: Missing output file path.");
+    }
 });
 
 app.listen(port, () => {
     console.log('Video processing service listening at http://localhost:' + port);
 });
+
