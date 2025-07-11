e4e8e87cf26d11115b40724d2f851f72bdd51f59
Msierra001 <msierra001@citymail.cuny.edu>
2025-07-10 22:43:00 -0400

    Changing from VideoPath to FilePath for better readability

--- video_processing_service/src/index.ts
+++ video_processing_service/src/index.ts
@@ -6,8 +6,10 @@ const port = 3000;      // specifying which port we want to use
 
 app.post("/process-video" , (req, res) => {
     // inputVideoPath and outputVideoPath get path input/output of the video we're inputting
-    const inputVideoPath = req.body.inputVideoPath;
-    const outputVideoPath = req.body.outputVideoPath; 
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath; 
+
+
 });
 
 app.listen(port, () => {
